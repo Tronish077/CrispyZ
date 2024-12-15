3fc55dd09e6f83546898e308e7d36c9454a70e71
iamTronish <tronishdevs177@gmail.com>
2024-12-15 16:29:51 +0530

    added a loader on item's page

--- src/UI/Itemsplace.jsx
+++ src/UI/Itemsplace.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import Cardproduct from "../Components/Cardproduct";
+import { MutatingDots, Puff } from "react-loader-spinner";
 import { apiinfo } from "../Contexts/apiinfo";
 
 
@@ -15,17 +16,36 @@ function Itemsplace(){
         
     }, [apidata]);
 
+    if(apidata.length > 2){
 
-    return(<div className="Itemsplace flex  gap-6">
-                {
-                    items.map((item)=>{
-                        return(
-                            <Cardproduct key={item.id} name={item.name} price={item.price} image={item.imagesrc}/>
-                        )
-                    })
-                }
+                return(<div className="Itemsplace flex  gap-6">
+                            { 
+                                items.map((item)=>{
+                                    return(
+                                        <Cardproduct key={item.id} name={item.name} price={item.price} image={item.imagesrc}/>
+                                    )
+                                })
+                            }
+                        
+                    </div>)
+            }else{
+
+        return(<div className="Itemsplace flex flex-col items-center justify-center  gap-6">
+                                <MutatingDots
+                    visible={true}
+                    height="100"
+                    width="100"
+                    color="rgb(220 38 38)"
+                    secondaryColor="black"
+                    radius="11.5"
+                    ariaLabel="mutating-dots-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    />
+
+                    <p className="text-red-600 text-lg">Fetching Products . . .</p>
         </div>)
-}
+    }}
 
 
 export default Itemsplace;
