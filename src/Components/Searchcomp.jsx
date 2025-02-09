c3395684cfcd2e434ccf9fd5dbdde6cf0cd9230d
iamTronish <tronishdevs177@gmail.com>
2025-02-09 23:42:32 +0530

    Search Complete

--- src/Components/Searchcomp.jsx
+++ src/Components/Searchcomp.jsx
@@ -23,9 +23,9 @@ function Searchcomp(){
 
                 <input type="search" id="srcbox"  className="srcbox shadow" placeholder="Search products . . ."/>
 
-                <Link to="#" className="text-black bg-white p-1 px-2 rounded flex items-center shadow">
+                <button onClick={()=>window.location.reload()} className="text-black bg-white p-1 px-2 rounded flex items-center shadow">
                     <span className="material-symbols-rounded">refresh</span>
-                </Link>
+                </button>
         </form>)
 }
 
